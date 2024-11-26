dff1aede74c82bba4e7cf54df806a3b603b3a116
Your Name <your@example.com>
2024-11-26 21:49:14 +0800

    Fix Firebase credentials issue

--- server.js
+++ server.js
@@ -52,7 +52,8 @@ app.use(express.json());
 const firebaseCredentials = {
   type: process.env.FIREBASE_TYPE,
   projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID.replace(/\\n/g, '\n'),
+  privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   clientId: process.env.FIREBASE_CLIENT_ID,
   authUri: process.env.FIREBASE_AUTH_URI,
